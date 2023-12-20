914949c168aeebb4ca8a4a50756f4357cb766e49
Nadir KHOUANI <khouani.nadir7@gmail.com>
2023-12-20 12:47:24 +0100

    debug image

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Logo.css";
-
+import logoburger from "../../assets/logoP2/LogoP2_large.png";
 export default function Logo() {
   return (
     <div>
       <img
         className="logoSection"
-        src="/logoP2/LogoP2_large.png"
+        src={logoburger}
         alt="Logo Deliver X"
       />
     </div>
