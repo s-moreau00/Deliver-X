abb00ffda5473aa3988350ed74e5b4a083033b50
ArthurVS05 <arthur.vincentsil@gmail.com>
2024-01-10 16:13:23 +0100

    debug

--- frontend/src/components/Paiement/PaiementDone/PaiementDone.jsx
+++ frontend/src/components/Paiement/PaiementDone/PaiementDone.jsx
@@ -6,8 +6,10 @@ export default function PaiementDone() {
     <main className="paiement-done-container">
       <h1>Merci pour votre commande !</h1>
 
-      <p>L'équipe prépare vos burgers avec soin.</p>
-      <p>On se retrouve très vite dans votre point de retrait.</p>
+      <p className="first-p">L'équipe prépare vos burgers avec soin.</p>
+      <p className="second-p">
+        On se retrouve très vite dans votre point de retrait.
+      </p>
       <LogoAnim />
     </main>
   );
